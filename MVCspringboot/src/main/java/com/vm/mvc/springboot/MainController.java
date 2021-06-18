b1e0205a51e61addf3fda987a73821aa2af53300
SHASHANK <shashankbompally@gmail.com>
2021-06-18 16:58:27 +0530

    updated commit

---
+++ MVCspringboot/src/main/java/com/vm/mvc/springboot/MainController.java
@@ -0,0 +1,26 @@
+package com.vm.mvc.springboot;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+    @RequestMapping("/")
+    public String index(
+        @RequestParam(value = "participant", required = false) String participant,
+        @RequestParam(value = "country", required = false) String country,
+        @RequestParam(value = "action", required = false) String action,
+        @RequestParam(value = "id", required = false) Integer id,
+        Model model
+    ) {
+        model.addAttribute("id", id);
+        List<Integer> userIds = Arrays.asList(1,2,3,4);
+        model.addAttribute("userIds", userIds);
+        return "index";
+    }
+}
